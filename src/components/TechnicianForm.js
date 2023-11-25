cabc9eb3184c24d43efabc88ad25353e893c9487
jrrodrigo421 <rjr89000@gmail.com>
2023-11-25 14:28:13 -0300

    registering technician

--- src/components/TechnicianForm.js
+++ src/components/TechnicianForm.js
@@ -1,30 +1,90 @@
-// // src/components/TechnicianForm.js
-// import React, { useState } from 'react';
-// import axios from 'axios';
 
-// const TechnicianForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     telephone: '',
-//     email: '',
-//     address: '',
-//   });
+import React, { useState } from 'react';
+import axios from 'axios';
 
-//   const handleChange = (e) => {
-//     // Implemente a lógica de atualização do estado do formulário
-//   };
+const TechnicianForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    telephone: '',
+    email: '',
+    address: '',
+  });
+  const [error, setError] = useState(null);
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     // Implemente a lógica para enviar os dados do formulário à API (POST)
-//   };
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-//   return (
-//     <div>
-//       <h1>Add Technician</h1>
-//       {/* Renderize o formulário aqui */}
-//     </div>
-//   );
-// };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-// export default TechnicianForm;
+    const formattedData = { ...formData, telephone: parseInt(formData.telephone, 10) };
+
+    try {
+      // Enviar os dados do formulário para a API (POST)
+      const response = await axios.post('http://localhost:3030/technicians', formattedData);
+
+      console.log('Técnico cadastrado com sucesso:', response.data);
+
+      // Limpar o formulário após o envio bem-sucedido
+      setFormData({
+        name: '',
+        telephone: '',
+        email: '',
+        address: '',
+      });
+      setError(null);
+    } catch (error) {
+      console.error('Erro ao cadastrar técnico:', error);
+      // Adicione tratamento de erro, se necessário
+      if (error.response) {
+        setError(error.response.data.message || 'Erro ao processar a solicitação.');
+      } else {
+        setError(error.message || 'Erro ao processar a solicitação.');
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Add Technician</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+        </label>
+        <br />
+        <label>
+          Telephone:
+          <input
+            type="number"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            name="telephone"
+            value={formData.telephone}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </label>
+        <br />
+        <label>
+          Address:
+          <input type="text" name="address" value={formData.address} onChange={handleChange} required />
+        </label>
+        <br />
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default TechnicianForm;
